3c3a066e8a3c5afc292facb079a20534ebf200cf
Arci <arciatanthoria@gmail.com>
2015-10-25 16:25:40 +0100

    Security: reworked passwords a little more

--- DataAccess/Data/UserInfoData.cs
+++ DataAccess/Data/UserInfoData.cs
@@ -12,6 +12,5 @@ namespace DataAccess.Data
         public string Email { get; set; }
 
         public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
-
     }
 }
--- DataAccess/Security/PasswordHelper.cs
+++ DataAccess/Security/PasswordHelper.cs
@@ -12,43 +12,30 @@ namespace DataAccess.Security
 
         public static PasswordData CreateHash(string password)
         {
-            byte[] salt;
-            byte[] subkey;
             using (var deriveBytes = new Rfc2898DeriveBytes(password, SaltSize, PBKDF2IterCount))
             {
-                salt = deriveBytes.Salt;
-                subkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
-            }
+                byte[] salt = deriveBytes.Salt;
+                byte[] subkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
 
-            byte[] outputBytes = new byte[1 + SaltSize + PBKDF2SubkeyLength];
-            Buffer.BlockCopy(salt, 0, outputBytes, 1, SaltSize);
-            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + SaltSize, PBKDF2SubkeyLength);
-            return new PasswordData()
-            {
-                PasswordHash = Convert.ToBase64String(outputBytes),
-                PasswordSalt = Convert.ToBase64String(salt)
-            };
+                return new PasswordData()
+                {
+                    PasswordHash = Convert.ToBase64String(subkey),
+                    PasswordSalt = Convert.ToBase64String(salt)
+                };
+            }
         }
 
         public static bool VerifyHashedPassword(string hashedPassword, string salt, string password)
         {
             byte[] hashedPasswordBytes = Convert.FromBase64String(hashedPassword);
-
-            // Wrong length or version header.
-            if (hashedPasswordBytes.Length != (1 + SaltSize + PBKDF2SubkeyLength) || hashedPasswordBytes[0] != 0x00)
-                return false;
-
             byte[] saltBytes = Convert.FromBase64String(salt);
-            Buffer.BlockCopy(hashedPasswordBytes, 1, saltBytes, 0, SaltSize);
-            byte[] storedSubkey = new byte[PBKDF2SubkeyLength];
-            Buffer.BlockCopy(hashedPasswordBytes, 1 + SaltSize, storedSubkey, 0, PBKDF2SubkeyLength);
 
-            byte[] generatedSubkey;
             using (var deriveBytes = new Rfc2898DeriveBytes(password, saltBytes, PBKDF2IterCount))
             {
-                generatedSubkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
+                byte[] generatedSubkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
+                return hashedPasswordBytes.SequenceEqual(generatedSubkey);
             }
-            return storedSubkey.SequenceEqual(generatedSubkey);
+            
         }
     }
 }
